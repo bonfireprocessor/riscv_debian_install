28164a0bcfab71d0975a1f87d3376c1b54bfc9dd
Thomas <thomas@bonfirecpu.eu>
2025-07-19 14:47:50 +0200

    add losetup path to env.sh (because /usr/sbin is not in PATH of  standard users anymore

--- env.sh
+++ env.sh
@@ -2,5 +2,5 @@ export IMAGE="${IMAGE:-$(realpath run/sdcard.img)}"
 export MOUNT_EFI="${MOUNT_EFI:-$(realpath mnt/bootfs)}"
 export MOUNT_ROOT="${MOUNT_ROOT:-$(realpath mnt/rootfs)}"
 
-LOOPDEV=$(losetup -j "$IMAGE" | awk -F: '{print $1}')
+LOOPDEV=$(/usr/sbin/losetup -j "$IMAGE" | awk -F: '{print $1}')
 export LOOPDEV
